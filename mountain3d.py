48c329f50115aab040a3f2b42c4d5f48ce6aa2be
itoshin-tech <itomakoto3141592@gmail.com>
2020-08-10 10:34:57 +0900

    ここからクラス化を試みる

--- mountain3d.py
+++ mountain3d.py
@@ -3,10 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# 作成サイズは漸化式の関係から 2**n + 1 と求まる
-# a1 = 3
-# a2 = 2 * a1 - 1
-# an = 2**n + 1
+# 領域サイズは漸化式の関係から 2**nn + 1 と導ける
+# nn は自然数でサイズレベルと呼ぶことにする
 
 # 左上を基準として、左上、右上、左下、右下の方向ベクトルを定義
 di = np.array([[0, 0], [1, 0], [0, 1], [1, 1]], dtype=np.uint8)  # dx, dy
@@ -36,30 +34,18 @@ def mountain(si, nn=3, alpha=0.1):
     ff[cy, cx] = center_val
     
     # ４辺の中点の値を代入、隣接の2隅と中央の値の平均+noise
-    # up
-    edge_val = (val[0] + val[1] +center_val) / 3 + np.random.randn() * ss * alpha
-    cx = int(si[0] + 0.5 * (ss - 1)) 
-    cy = int(si[1])
-    if ff[cy, cx] == 0:
-        ff[cy, cx] = edge_val
-    # left
-    edge_val = (val[0] + val[2] +center_val) / 3 + np.random.randn() * ss * alpha
-    cx = int(si[0]) 
-    cy = int(si[1] + 0.5 * (ss - 1))
-    if ff[cy, cx] == 0:
-        ff[cy, cx] = edge_val
-    # right
-    edge_val = (val[1] + val[3] +center_val) / 3 + np.random.randn() * ss * alpha
-    cx = int(si[0] + (ss - 1)) 
-    cy = int(si[1] + 0.5 * (ss - 1))
-    if ff[cy, cx] == 0:
-        ff[cy, cx] = edge_val
-    # down
-    edge_val = (val[2] + val[3] +center_val) / 3 + np.random.randn() * ss * alpha
-    cx = int(si[0] + 0.5 * (ss - 1)) 
-    cy = int(si[1] + (ss - 1))
-    if ff[cy, cx] == 0:
-        ff[cy, cx] = edge_val
+    p0 = [0, 0, 1, 2]
+    p1 = [1, 2, 3, 3]
+    m0 = [0.5, 0, 1, 0.5]
+    m1 = [0, 0.5, 0.5, 1]
+    for i in range(4):
+        # 中点の座標
+        cx = int(si[0] + m0[i] * (ss - 1)) 
+        cy = int(si[1] + m1[i] * (ss - 1))
+        # 代入する値は対応する両端と中心の値+noise
+        edge_val = (val[p0[i]] + val[p1[i]] +center_val) / 3 + np.random.randn() * ss * alpha
+        if ff[cy, cx] == 0:
+            ff[cy, cx] = edge_val
     
     # 4つの小領域（左上、右上、左下、右下）で山を生成
     for i in range(4):
@@ -70,7 +56,7 @@ def mountain(si, nn=3, alpha=0.1):
 # ------- main 
 
 # サイズ
-nn = 8
+nn = 6
 ss = 2 ** nn + 1 
 
 # フィールド
@@ -80,7 +66,7 @@ ff = np.zeros((ss, ss))
 si = np.array([0, 0], dtype=np.uint8)
 
 # 山の生成
-mountain(si, nn, alpha=0.05)
+mountain(si, nn, alpha=0.15)
 
 # 描画
 fig = plt.figure(figsize=(10, 5))
@@ -93,6 +79,6 @@ xx, yy = np.meshgrid(x, y)
 ax2.plot_surface(xx, yy, ff, cmap='gist_earth')
 ax2.set_zlim(-20, 20)
 ax2.set_ylim(10, -10)
-ax2.view_init(60 ,-80)
+ax2.view_init(40 ,-80)
 ax2.axis("off")
 plt.show()
