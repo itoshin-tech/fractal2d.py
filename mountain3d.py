64607f9ed3cd68ac9a75faa0765634e010e0ce3b
itoshin-tech <itomakoto3141592@gmail.com>
2020-08-10 09:55:44 +0900

    改良前

--- mountain3d.py
+++ mountain3d.py
@@ -1,19 +1,31 @@
 #code: utf-8
 import numpy as np
 import matplotlib.pyplot as plt
-import pdb
+from mpl_toolkits.mplot3d import Axes3D
 
-# サイズは漸化式で求まる
+# 作成サイズは漸化式の関係から 2**n + 1 と求まる
 # a1 = 3
 # a2 = 2 * a1 - 1
 # an = 2**n + 1
+
+# 左上を基準として、左上、右上、左下、右下の方向ベクトルを定義
 di = np.array([[0, 0], [1, 0], [0, 1], [1, 1]], dtype=np.uint8)  # dx, dy
 
-def update(si, nn=3, alpha=0.1):
+# 山の生成関数
+def mountain(si, nn=3, alpha=0.1):
+    """
+    si: 生成する領域の左上の座標
+    nn: 生成するサイズレベル (サイズは 2**nn + 1)
+    """
+
+    # サイズレベルが1未満なら何もしない
     if nn < 1:
         return
+
+    # 生成する領域サイズ
     ss = 2 ** nn + 1
-    # 中心の値を入力
+
+    # 中心の値を代入、領域の4隅の値の平均＋noise
     val = np.zeros(4)
     for i in range(4):
         ci = si + di[i] * (ss - 1) # y, x
@@ -23,7 +35,7 @@ def update(si, nn=3, alpha=0.1):
     cy = int(si[1] + 0.5 * (ss - 1))
     ff[cy, cx] = center_val
     
-    # 辺の中点の値を入力
+    # ４辺の中点の値を代入、隣接の2隅と中央の値の平均+noise
     # up
     edge_val = (val[0] + val[1] +center_val) / 3 + np.random.randn() * ss * alpha
     cx = int(si[0] + 0.5 * (ss - 1)) 
@@ -49,31 +61,38 @@ def update(si, nn=3, alpha=0.1):
     if ff[cy, cx] == 0:
         ff[cy, cx] = edge_val
     
-    # 4つの領域
-
+    # 4つの小領域（左上、右上、左下、右下）で山を生成
     for i in range(4):
         ssi = si + di[i] * (ss - 1) * 0.5
         ssi = ssi.astype(np.int)
-        update(ssi, nn - 1, alpha=alpha)
+        mountain(ssi, nn - 1, alpha=alpha)
 
-# test         
-from mpl_toolkits.mplot3d import Axes3D
+# ------- main 
 
-nn = 7
-ss = 2 ** nn + 1
+# サイズ
+nn = 8
+ss = 2 ** nn + 1 
+
+# フィールド
 ff = np.zeros((ss, ss))
+
+# 基準座標
 si = np.array([0, 0], dtype=np.uint8)
-update(si, nn, alpha=0.1)
-fig = plt.figure(figsize=(12, 6))
 
-ax = fig.add_subplot(1, 3, 1)
+# 山の生成
+mountain(si, nn, alpha=0.05)
+
+# 描画
+fig = plt.figure(figsize=(10, 5))
+ax = fig.add_subplot(1, 4, 1)
 ax.imshow(ff, cmap='gist_earth')
-
-ax2 = fig.add_subplot(1, 3, (2, 3), projection='3d')
-# ax2.set_aspect('equal')
+ax2 = fig.add_subplot(1, 4, (2, 4), projection='3d')
 x = np.linspace(-10, 10, ss)
 y = np.linspace(-10, 10, ss)
 xx, yy = np.meshgrid(x, y)
 ax2.plot_surface(xx, yy, ff, cmap='gist_earth')
 ax2.set_zlim(-20, 20)
+ax2.set_ylim(10, -10)
+ax2.view_init(60 ,-80)
+ax2.axis("off")
 plt.show()
